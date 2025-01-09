38ac83d019e919eb70eca9f558735ed6f05effc7
SaiVeena258 <saiveenakurella25@gmail.com>
2025-01-09 21:58:32 +0530

    Basic print in java is implemented

---
+++ Getting Started/YourInitials.java
@@ -0,0 +1,11 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        System.out.println(" SSS   V   V");
+        System.out.println("S   S  V   V");
+        System.out.println("S      V   V");
+        System.out.println(" SSS   V   V");
+        System.out.println("    S  V   V");
+        System.out.println("S   S   V V ");
+        System.out.println(" SSS     V  ");
+    }
+}
