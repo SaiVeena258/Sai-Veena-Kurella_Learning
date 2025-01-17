00b0a297f17f4e2ea092304e5f7faabf13a976a2
sai <saiveenakurella25@gmail.com>
2025-01-17 23:55:27 +0530

    Small applications on variables and conditions

--- Variables and Conditionals/Details.java
+++ Variables and Conditionals/Details.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Details{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Please give us your details...");;
         System.out.println("Enter your name:");
         String name=sc.nextLine();
         System.out.println("Enter your age:");
